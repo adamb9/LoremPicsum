ebc10587588fe0a51af2f35eca932693c30047ee
Adam Baldwin <adambaldwin9@gmail.com>
2025-03-05 22:55:25 +0000

    Added UI for dropdown list filter

--- app/src/main/java/com/example/lorempicsum/MainActivity.kt
+++ app/src/main/java/com/example/lorempicsum/MainActivity.kt
@@ -6,14 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.lorempicsum.data.api.ImageApiServiceImpl
-import com.example.lorempicsum.repo.ImageRepoImpl
-import com.example.lorempicsum.ui.composables.ImageList
+import com.example.lorempicsum.ui.screens.ImageListScreen
 import com.example.lorempicsum.ui.theme.LoremPicsumTheme
-import kotlinx.coroutines.runBlocking
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.GlobalContext.startKoin
@@ -35,32 +31,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             LoremPicsumTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Surface(modifier = Modifier.padding(innerPadding)) {
+                        ImageListScreen()
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    val apiService = ImageApiServiceImpl()
-    val repo = ImageRepoImpl(apiService)
-    val imageList = runBlocking {
-        repo.getImageList(1, 50)
-    }
-
-    ImageList(imageList)
-
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LoremPicsumTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/lorempicsum/ui/composables/DropdownMenuFilterRow.kt
@@ -0,0 +1,94 @@
+package com.example.lorempicsum.ui.composables
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.lorempicsum.R
+
+@Composable
+fun DropdownMenuFilterRow(names: List<String>) {
+    val isDropDownExpanded = remember {
+        mutableStateOf(false)
+    }
+
+    val selectedName = remember {
+        mutableStateOf("")
+    }
+
+    Row(
+        modifier = Modifier
+            .padding(16.dp, 8.dp)
+            .wrapContentHeight()
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Text(
+            text = stringResource(R.string.filter_label),
+            modifier = Modifier.weight(1f)
+        )
+        Box(
+            modifier = Modifier
+                .padding(8.dp, 0.dp)
+                .weight(4f)
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable {
+                        isDropDownExpanded.value = true
+                    }
+            ) {
+                Text(text = selectedName.value)
+                Image(
+                    painter = painterResource(id = R.drawable.drop_down_ic),
+                    contentDescription = stringResource(R.string.dropdown_icon)
+                )
+            }
+            DropdownMenu(
+                expanded = isDropDownExpanded.value,
+                onDismissRequest = {
+                    isDropDownExpanded.value = false
+                }
+            ) {
+                names.forEach {
+                    DropdownMenuItem(
+                        text = { Text(text = it) },
+                        onClick = {
+                            isDropDownExpanded.value = false
+                            selectedName.value = it
+                        }
+                    )
+                }
+            }
+        }
+        Button(
+            onClick = {
+                selectedName.value = ""
+            },
+            modifier = Modifier.weight(2f)
+        ) {
+            Text(text = stringResource(R.string.clear))
+        }
+
+    }
+}
