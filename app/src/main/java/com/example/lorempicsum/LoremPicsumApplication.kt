6c98a76db8758f627a7005eba637c5f1467985dc
Adam Baldwin <adambaldwin9@gmail.com>
2025-03-06 20:43:18 +0000

    Changed disk and memory image caching settings

--- app/src/main/java/com/example/lorempicsum/LoremPicsumApplication.kt
+++ app/src/main/java/com/example/lorempicsum/LoremPicsumApplication.kt
@@ -1,12 +1,17 @@
 package com.example.lorempicsum
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
+import coil.request.CachePolicy
 import com.example.lorempicsum.di.appModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
-class LoremPicsumApplication : Application() {
+class LoremPicsumApplication : Application(), ImageLoaderFactory {
     override fun onCreate() {
         super.onCreate()
 
@@ -17,5 +22,25 @@ class LoremPicsumApplication : Application() {
                 appModule
             )
         }
+    }
+
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader(this).newBuilder()
+            .memoryCachePolicy(CachePolicy.ENABLED)
+            .memoryCache {
+                MemoryCache.Builder(this)
+                    .maxSizePercent(0.05)
+                    .strongReferencesEnabled(true)
+                    .build()
+            }
+            .diskCachePolicy(CachePolicy.ENABLED)
+            .diskCache {
+                DiskCache.Builder()
+                    .maxSizePercent(0.05)
+                    .directory(cacheDir)
+                    .build()
+            }
+            //.logger(DebugLogger())
+            .build()
     }
 }
